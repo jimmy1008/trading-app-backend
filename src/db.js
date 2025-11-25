3ca66c0430cdba7e5a13b66e2d8ae80fdbdf679a
Naxy-xxx <ed950625950625@gmail.com>
2025-11-25 13:35:00 +0800

    7854916

--- src/db.js
+++ src/db.js
@@ -1,11 +1,9 @@
 import pkg from 'pg';
 const { Pool } = pkg;
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
+const isProduction = process.env.NODE_ENV === 'production';
 
-export { pool };
+export const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: isProduction ? { rejectUnauthorized: false } : false
+});
